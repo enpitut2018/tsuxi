f00bfe0171142fd01b2a9c7683058d6765d997f6
bridgebooktakuya <t.hashimo.s@gmail.com>
2018-07-25 15:41:58 +0900

    chage to keepable message to db

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -9,6 +9,7 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(data)
    ActionCable.server.broadcast 'room_channel', message: data['message']
-  end
+   Message.create! content: data['message']
+ end
 
 end
