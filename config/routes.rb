ae8cabd651ab78fec1abb60616baf395865ca7ad
tsuguhisa <roilvder1122@gmail.com>
2018-07-26 16:57:05 +0900

    joincommunity

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :edit, :create, :update]
   resources :sessions, only: [:new, :create, :destroy]
   resources :communities
+  resources :communities do
+  	post 'add' => 'joins#create'
+  	delete 'delete' => 'joins#destroy'
+  end
 
   match 'signup', to: 'users#new', via: 'get'
   match 'signin', to: 'sessions#new', via: 'get'
   match 'signout', to: 'sessions#destroy', via: 'delete'
 
   root 'users#index'
+
+
 end
