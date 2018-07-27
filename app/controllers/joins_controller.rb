663393a654088270303ec91523f23ee140a725a1
IkumaOka <ikuma1018soccer06@example.com>
2018-07-27 16:14:29 +0900

    debug

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -6,9 +6,9 @@ class JoinsController < ApplicationController
     if Join.where(community_id: params[:community_id]).count >= 1
       flash[:notice] = "#{community.name}に参加できません"
       redirect_to communities_path
-    #elsif User.where(sex: params[:sex]) == '女'
-      #flash[:alert] = "あなたは女性なので参加できません"
-      #redirect_to communities_path
+      # elsif User.where(sex: params[:sex]) == '女'
+      # flash[:alert] = "あなたは女性なので参加できません"
+      # redirect_to communities_path
     elsif Join.create(user_id: user.id,community_id:community.id)
       redirect_to community
     else
