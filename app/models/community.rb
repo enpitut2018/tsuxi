9d6ad1beb43e1ccc6c788b056d4a43cac2814def
IkumaOka <ikuma1018soccer06@example.com>
2018-07-27 15:27:42 +0900

    add validates about community

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,7 @@
 class Community < ApplicationRecord
 	has_many :joins
-    has_many :users, through: :joins
+  has_many :users, through: :joins
+
+  validates :name,    presence: true # 名前空だめ
+  validates :content, presence: true # 概要空だめ
 end
