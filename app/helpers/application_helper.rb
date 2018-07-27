384b3ce6c593b65ff30bc53ac9a89edf9b1e5170
IkumaOka <ikuma1018soccer06@example.com>
2018-07-27 14:34:31 +0900

    modify titile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def page_title
+    title = "Tsuxi"
+    title = @page_title + " | " + title if @page_title
+
+    return title
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ActiveRecord::Base
     has_many :joins
     has_many :communities, through: :joins
-    
-
-   #validate :check_number_of_users
-
-   #def check_number_of_users
-
-    # if join.user && join.community.count > 0
-     #  errors.add(:community, "人数OVER")
-     #end
-   #end
 
     #名前
     validates :name,  presence: true, length: { maximum: 50 }
+
     #メール
     VALID_EMAIL_REGEX = /\As[0-9]{7}+@[u|s].tsukuba.ac.jp/
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
